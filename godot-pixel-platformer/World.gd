5894689ff5f86aebedec0c59aca4cfac79592bbc
Haoyu Qiu <timothyqiu32@gmail.com>
2020-04-02 23:01:01 +0800

    Adds simple scene transition

--- godot-pixel-platformer/World.gd
+++ godot-pixel-platformer/World.gd
@@ -1,4 +1,9 @@
 extends Node2D
 
+onready var animationPlayer = $CanvasLayer/AnimationPlayer
+
 func _on_Exit_body_entered(body):
+	animationPlayer.play("TransitionOut")
+
+func transition_out_finished():
 	get_tree().reload_current_scene()
