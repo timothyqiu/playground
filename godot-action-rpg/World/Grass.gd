f6ba6a59964f92ac98eb209b2fb534830e173c81
Haoyu Qiu <timothyqiu32@gmail.com>
2020-04-05 14:08:02 +0800

    Hurtbox and hitboxes

--- godot-action-rpg/World/Grass.gd
+++ godot-action-rpg/World/Grass.gd
@@ -2,10 +2,9 @@ extends Node2D
 
 var GrassEffect = load("res://Effects/GrassEffect.tscn")
 
-func _process(delta):
-	if Input.is_action_just_pressed("attack"):
-		var grassEffect = GrassEffect.instance()
-		grassEffect.global_position = global_position
-		var world = get_tree().current_scene
-		world.add_child(grassEffect)
-		queue_free()
+func _on_Hurtbox_area_entered(area):
+	var grassEffect = GrassEffect.instance()
+	grassEffect.global_position = global_position
+	var world = get_tree().current_scene
+	world.add_child(grassEffect)
+	queue_free()
