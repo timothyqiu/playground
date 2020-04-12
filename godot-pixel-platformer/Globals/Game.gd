0013fb9faaccc1900c166e1d2a8fdfa03c2d5de7
Haoyu Qiu <timothyqiu32@gmail.com>
2020-04-12 16:19:25 +0800

    Adds coins

--- godot-pixel-platformer/Globals/Game.gd
+++ godot-pixel-platformer/Globals/Game.gd
@@ -9,6 +9,9 @@ const levels = [
 var game_started_at: int
 var game_completed_at: int
 var total_deaths: int = 0
+var coins_total: int = 0
+var coins_collected: int = 0
+var coins_current_level: int = 0  # buffer for coins collected in this level
 
 var current_level: int = 2
 
@@ -31,10 +34,15 @@ func go_first_level():
 	game_started_at = OS.get_unix_time()
 	game_completed_at = -1
 	total_deaths = 0
+	coins_total = 0
+	coins_collected = 0
 	_go_level(0)
 
 
 func go_next_level():
+	coins_collected += coins_current_level
+	coins_total += get_tree().current_scene.coins_total
+	
 	var next_level = current_level + 1
 	if next_level == levels.size():
 		game_completed_at = OS.get_unix_time()
@@ -46,4 +54,5 @@ func go_next_level():
 func _go_level(level: int):
 	assert(0 <= level and level < levels.size())
 	SceneTransition.transition_to(levels[level])
+	coins_current_level = 0
 	current_level = level
--- godot-pixel-platformer/UI/GameComplete.gd
+++ godot-pixel-platformer/UI/GameComplete.gd
@@ -8,7 +8,11 @@ func _ready():
 	var total_seconds = Game.game_completed_at - Game.game_started_at
 	var minutes = total_seconds / 60
 	var seconds = total_seconds % 60
-	stats.text = "%02d:%02d\n%d" % [minutes, seconds, Game.total_deaths]
+	stats.text = "%02d:%02d\n%d / %d\n%d\n" % [
+		minutes, seconds,
+		Game.coins_collected, Game.coins_total,
+		Game.total_deaths,
+	]
 
 
 func _unhandled_input(event):
