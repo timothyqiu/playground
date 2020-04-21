e571e6b321cd7c704f55f121d805ecf8a8b91334
Haoyu Qiu <timothyqiu32@gmail.com>
2020-04-21 14:15:49 +0800

    Adds health UI and shadows

--- godot-action-rpg/Stats.gd
+++ godot-action-rpg/Stats.gd
@@ -1,14 +1,27 @@
 extends Node
 
-signal no_health
+signal health_changed(value)
+signal max_health_changed(value)
+signal no_health()
 
-export var max_health = 1
+export var max_health = 1 setget set_max_health
 
-onready var health = max_health setget set_health
+var health = 0 setget set_health
+
+
+func _ready() -> void:
+	self.health = max_health
 
 
 func set_health(value):
 	health = value
+	emit_signal("health_changed", health)
 	
 	if health <= 0:
 		emit_signal("no_health")
+
+
+func set_max_health(value):
+	max_health = value
+	self.health = min(health, max_health)
+	emit_signal("max_health_changed", max_health)
