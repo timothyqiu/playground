4feeea3e9f240bb2d0f844cd6f5f93445a4e2bd5
Haoyu Qiu <timothyqiu32@gmail.com>
2023-11-16 15:52:10 +0800

    Create logical device

--- vulkan-zig/src/main.zig
+++ vulkan-zig/src/main.zig
@@ -12,8 +12,8 @@ fn getLibraryFunc(library: *std.DynLib, comptime name: [:0]const u8) !ApiFuncTyp
     return library.lookup(ApiFuncType(name), name) orelse error.AddrNotFound;
 }
 
-fn getVulkanFunc(comptime name: [:0]const u8, proc_addr: anytype, handle: anytype) !ApiFuncType(name) {
-    return @as(?ApiFuncType(name), @ptrCast(proc_addr(handle, name))) orelse error.AddrNotFound;
+fn getVulkanCoreFunc(comptime name: [:0]const u8, proc_addr: anytype, handle: anytype) ApiFuncType(name) {
+    return @ptrCast(proc_addr(handle, name).?);
 }
 
 const Entry = struct {
@@ -43,12 +43,11 @@ const Entry = struct {
         errdefer library.close();
 
         const get_instance_proc_addr = try getLibraryFunc(&library, "vkGetInstanceProcAddr");
-        const create_instance = try getVulkanFunc("vkCreateInstance", get_instance_proc_addr, null);
 
         return .{
             .handle = library,
             .get_instance_proc_addr = get_instance_proc_addr,
-            .create_instance = create_instance,
+            .create_instance = getVulkanCoreFunc("vkCreateInstance", get_instance_proc_addr, null),
         };
     }
 
@@ -71,6 +70,8 @@ const Instance = struct {
     destroy_instance: std.meta.Child(c.PFN_vkDestroyInstance),
     enumerate_physical_devices: std.meta.Child(c.PFN_vkEnumeratePhysicalDevices),
     get_physical_device_queue_family_properties: std.meta.Child(c.PFN_vkGetPhysicalDeviceQueueFamilyProperties),
+    create_device: std.meta.Child(c.PFN_vkCreateDevice),
+    get_device_proc_addr: std.meta.Child(c.PFN_vkGetDeviceProcAddr),
     allocation_callbacks: ?*c.VkAllocationCallbacks,
 
     fn init(entry: Entry, allocation_callbacks: ?*c.VkAllocationCallbacks) !Self {
@@ -82,10 +83,12 @@ const Instance = struct {
         return switch (entry.create_instance(&info, allocation_callbacks, &instance)) {
             c.VK_SUCCESS => .{
                 .handle = instance,
-                .destroy_instance = try getVulkanFunc("vkDestroyInstance", entry.get_instance_proc_addr, instance),
                 .allocation_callbacks = allocation_callbacks,
-                .enumerate_physical_devices = try getVulkanFunc("vkEnumeratePhysicalDevices", entry.get_instance_proc_addr, instance),
-                .get_physical_device_queue_family_properties = try getVulkanFunc("vkGetPhysicalDeviceQueueFamilyProperties", entry.get_instance_proc_addr, instance),
+                .destroy_instance = getVulkanCoreFunc("vkDestroyInstance", entry.get_instance_proc_addr, instance),
+                .enumerate_physical_devices = getVulkanCoreFunc("vkEnumeratePhysicalDevices", entry.get_instance_proc_addr, instance),
+                .get_physical_device_queue_family_properties = getVulkanCoreFunc("vkGetPhysicalDeviceQueueFamilyProperties", entry.get_instance_proc_addr, instance),
+                .create_device = getVulkanCoreFunc("vkCreateDevice", entry.get_instance_proc_addr, instance),
+                .get_device_proc_addr = getVulkanCoreFunc("vkGetDeviceProcAddr", entry.get_instance_proc_addr, instance),
             },
             c.VK_ERROR_OUT_OF_HOST_MEMORY => error.VulkanOutOfHostMemory,
             c.VK_ERROR_OUT_OF_DEVICE_MEMORY => error.VulkanOutOfDeviceMemory,
@@ -116,15 +119,13 @@ const Instance = struct {
         var physical_devices = try allocator.alloc(c.VkPhysicalDevice, count);
         errdefer allocator.free(physical_devices);
 
-        switch (self.enumerate_physical_devices(self.handle, &count, physical_devices.ptr)) {
-            c.VK_SUCCESS, c.VK_INCOMPLETE => {},
-            c.VK_ERROR_OUT_OF_HOST_MEMORY => return error.VulkanOutOfHostMemory,
-            c.VK_ERROR_OUT_OF_DEVICE_MEMORY => return error.VulkanOutOfDeviceMemory,
-            c.VK_ERROR_INITIALIZATION_FAILED => return error.VulkanInitializationFailed,
+        return switch (self.enumerate_physical_devices(self.handle, &count, physical_devices.ptr)) {
+            c.VK_SUCCESS, c.VK_INCOMPLETE => physical_devices,
+            c.VK_ERROR_OUT_OF_HOST_MEMORY => error.VulkanOutOfHostMemory,
+            c.VK_ERROR_OUT_OF_DEVICE_MEMORY => error.VulkanOutOfDeviceMemory,
+            c.VK_ERROR_INITIALIZATION_FAILED => error.VulkanInitializationFailed,
             else => unreachable,
-        }
-
-        return physical_devices;
+        };
     }
 
     fn selectQueueFamily(self: Self, physical_devices: []c.VkPhysicalDevice, mask: c.VkQueueFlags, allocator: std.mem.Allocator) !?QueueFamily {
@@ -142,7 +143,7 @@ const Instance = struct {
                 if (queue_family_property.queueFlags & mask != 0) {
                     return .{
                         .physical_device = device,
-                        .queue_family_index = queue_family_index,
+                        .queue_family_index = @intCast(queue_family_index),
                     };
                 }
             }
@@ -154,7 +155,52 @@ const Instance = struct {
 
 const QueueFamily = struct {
     physical_device: c.VkPhysicalDevice,
-    queue_family_index: usize,
+    queue_family_index: u32,
+};
+
+const Device = struct {
+    const Self = @This();
+
+    handle: c.VkDevice,
+    destroy_device: std.meta.Child(c.PFN_vkDestroyDevice),
+    allocation_callbacks: ?*c.VkAllocationCallbacks,
+
+    fn init(instance: Instance, queue_family: QueueFamily, allocation_callbacks: ?*c.VkAllocationCallbacks) !Device {
+        const queue_create_infos = [_]c.VkDeviceQueueCreateInfo{
+            std.mem.zeroInit(c.VkDeviceQueueCreateInfo, .{
+                .sType = c.VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO,
+                .queueFamilyIndex = queue_family.queue_family_index,
+                .queueCount = 1,
+                .pQueuePriorities = &[_]f32{1.0},
+            }),
+        };
+        const info = std.mem.zeroInit(c.VkDeviceCreateInfo, .{
+            .sType = c.VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO,
+            .queueCreateInfoCount = queue_create_infos.len,
+            .pQueueCreateInfos = &queue_create_infos,
+        });
+
+        var device: c.VkDevice = undefined;
+        return switch (instance.create_device(queue_family.physical_device, &info, allocation_callbacks, &device)) {
+            c.VK_SUCCESS => .{
+                .handle = device,
+                .allocation_callbacks = allocation_callbacks,
+                .destroy_device = getVulkanCoreFunc("vkDestroyDevice", instance.get_device_proc_addr, device),
+            },
+            c.VK_ERROR_OUT_OF_HOST_MEMORY => error.VulkanOutOfHostMemory,
+            c.VK_ERROR_OUT_OF_DEVICE_MEMORY => error.VulkanOutOfDeviceMemory,
+            c.VK_ERROR_INITIALIZATION_FAILED => error.VulkanInitializationFailed,
+            c.VK_ERROR_EXTENSION_NOT_PRESENT => error.VulkanExtensionNotPresent,
+            c.VK_ERROR_FEATURE_NOT_PRESENT => error.VulkanFeatureNotPresent,
+            c.VK_ERROR_TOO_MANY_OBJECTS => error.VulkanTooManyObjects,
+            c.VK_ERROR_DEVICE_LOST => error.VulkanDeviceLost,
+            else => unreachable,
+        };
+    }
+
+    fn deinit(self: Self) void {
+        self.destroy_device(self.handle, self.allocation_callbacks);
+    }
 };
 
 pub fn main() !void {
@@ -173,8 +219,10 @@ pub fn main() !void {
 
     const physical_devices = try instance.enumeratePhysicalDevices(allocator);
     defer allocator.free(physical_devices);
-
     const queue_family = try instance.selectQueueFamily(physical_devices, c.VK_QUEUE_COMPUTE_BIT, allocator) orelse return error.NoSuitablePhysicalDevice;
 
-    std.debug.print("Physical device: {}\n", .{queue_family});
+    const device = try Device.init(instance, queue_family, null);
+    defer device.deinit();
+
+    std.debug.print("Logical device: {}\n", .{device});
 }
